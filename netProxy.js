4c0d6ef8bdcce8a61263b82e18923440d4d78965
Westixy <westixy@gmail.com>
2018-05-15 12:03:41 +0200

    node ipc wrapper seems good

--- netProxy.js
+++ netProxy.js
@@ -1,117 +1,133 @@
 const uuidv4 = require('uuid/v4')
 const ipc = require('node-ipc')
-const debug = require('debug')
 
 const {
   NetClientProxy,
   NetServerProxy
 } = require('@lug/netproxy')
 
+const dataEvent = '_message_'
+
+ipc.config.id = '_net_proxy_'
+ipc.config.silent = true
 
 class NetClientProxyNodeIpc extends NetClientProxy {
   constructor() {
     super()
-    this._server = null
-    this._address = uuidv4()
-    this._timeout = 1000
-    this._onData = this._onData.bind(this)
+    this._ipcId = uuidv4()
+    this._ipc = ipc
+    this._emitter = null
+    this._state = null
+    this._dataEvent = dataEvent
   }
   get state() {
-    return this._server ? 'connected' : 'disconnected'
+    return this._state
   }
   _initEvents() {
     super._initEvents()
   }
   get socket() {
-    return this._server ? `${this._server}.${this._address}` : null
+    return null
   }
   connect(ip, port) {
-    let socket = `net.${port}.${this._address}`
-    let timeout = setTimeout(() => {
-      this.ev.emit('error', `Server not found ${ip}:${port}`)
-    }, this._timeout)
-    netProxyEmul.once(`${socket}.connected`, () => {
-      clearTimeout(timeout)
-      this._server = `net.${port}`
-      this.ev.emit('connected', this.socket)
-      netProxyEmul.on(this.socket, this._onData)
+    if (this.state !== null)
+      return this.ev.emit('error', Error('Cannot connect two times the same client without disconnect'))
+    ipc.connectToNet(this._ipcId, ip, port, () => {
+      this._emitter = ipc.of[this._ipcId]
+      this._initIpcEvents()
+      this.ev.emit('connect')
     })
-    netProxyEmul.emit(`net.${port}.connect`, socket)
+
+  }
+  _initIpcEvents() {
+    this._emitter.on('error', err => this.ev.emit('error', err))
+    this._emitter.on('connect', () => {
+      this._state = 'connected'
+      this.ev.emit('connected')
+    })
+    this._emitter.on('disconnect', () => {
+      this._state = 'disconnected'
+      this.ev.emit('disconnect')
+    })
+    this._emitter.on('destroy', () => {
+      this._state = null
+      this._emitter = null
+      this.ev.emit('destroy')
+    })
+    this._emitter.on(this._dataEvent, this._onData.bind(this))
   }
   _onData(data) {
     this.ev.emit('data', data)
   }
   disconnect() {
-    netProxyEmul.emit(`${this._server}.disconnect`, this.socket)
-    this.ev.emit('disconnect')
-    netProxyEmul.off(this.socket, this._onData)
-    this._server = null
-    this.ev.emit('destroy')
+    ipc.disconnect(this._ipcId)
   }
   send(data) {
-    netProxyEmul.emit(`${this._server}.data`, this.socket, data)
+    this._emitter.emit(this._dataEvent, data)
   }
 }
 
 class NetServerProxyNodeIpc extends NetServerProxy {
   constructor() {
     super()
-    this.port = null
+    this._port = null
+    this._emitter = null
+    this._state = null
     this._onConnect = this._onConnect.bind(this)
     this._onDisonnect = this._onDisonnect.bind(this)
     this._onData = this._onData.bind(this)
+    this._dataEvent = dataEvent
+    this._ipc = ipc
+    this._sockets = []
   }
   _initEvents() {
     super._initEvents()
   }
   get state() {
-    return this.port ? 'on' : 'off'
+    return this._state
   }
   listen(port) {
-    this.port = port
-    netProxyEmul.on(`net.${port}.connect`, this._onConnect)
-    netProxyEmul.on(`net.${port}.disconnect`, this._onDisonnect)
-    netProxyEmul.on(`net.${port}.data`, this._onData)
-    this.ev.emit('start')
-    this.waiter = setInterval(() => {}, 1000)
+    this._port = port
+    ipc.serveNet(port, () => {
+      this._state = 'listening'
+      this._emitter = ipc.server
+      this._initIpcEvents()
+      this.ev.emit('start')
+    })
+    ipc.server.start()
+  }
+  _initIpcEvents() {
+    this._emitter.on('connect', this._onConnect)
+    this._emitter.on('socket.disconnected', this._onDisonnect)
+    this._emitter.on(this._dataEvent, this._onData)
   }
   _onConnect(socket) {
-    let sock = this.sockets.find(s => s === socket)
+    let sock = this._sockets.find(s => s === socket)
     if (sock === undefined) {
-      this.sockets.push(socket)
+      this._sockets.push(socket)
       this.ev.emit('socket.connect', socket)
-    } else this.ev.emit('error', `Socket already connected : ${socket}`)
-    netProxyEmul.emit(`${socket}.connected`)
+    } else this.ev.emit('error', Error(`Socket already in list : ${socket}`))
   }
   _onDisonnect(socket) {
-    let index = this.sockets.indexOf(socket)
+    let index = this._sockets.findIndex(s => s === socket)
     if (index !== -1) {
-      this.sockets.splice(index, 1)
+      this._sockets.splice(index, 1)
       this.ev.emit('socket.disconnect', socket)
-    } else this.ev.emit('error', `Socket already disconnected : ${socket}`)
+    } else this.ev.emit('error', Error(`Socket not in list but fire a disconnect event: ${socket}`))
   }
-  _onData(socket, data) {
-    let sock = this.sockets.find(s => s === socket)
-    if (sock !== undefined) {
-      this.ev.emit('socket.data', socket, data)
-    } else
-      this.ev.emit(
-        'error',
-        `Socket try to send data but is not known : ${socket} ; ${data}`
-      )
+  _onData(data, socket) {
+    this.ev.emit('socket.data', socket, data)
   }
   stop() {
-    let port = this.port
-    this.port = null
-    this.sockets = []
-    netProxyEmul.off(`net.${port}.connect`, this._onConnect)
-    netProxyEmul.off(`net.${port}.disconnect`, this._onDisonnect)
-    netProxyEmul.off(`net.${port}.data`, this._onData)
+    this._emitter.stop()
+    this._emitter = null
+    this._port = null
+    this._sockets = []
+    this._state = null
     this.ev.emit('stop')
-    clearInterval(this.waiter)
   }
   send(socket, data) {
-    netProxyEmul.emit(socket, data)
+    ipc.server.emit(socket, this._dataEvent, data)
   }
 }
 
--- test/index.js
+++ test/index.js
@@ -6,16 +6,22 @@ const {
 const client = new NetClientProxyNodeIpc()
 const server = new NetServerProxyNodeIpc()
 
-server.listen(9999)
 
 server.ev.on('socket.data', (socket, data) => {
   server.send(socket, `I received your message : """${data}"""`)
 })
+server.ev.on('start', ()=>{
+  setTimeout(client.disconnect.bind(client),1000)
+  setTimeout(server.stop.bind(server),2500)
+})
+
+client.ev.on('connected', ()=>{
+  client.send('Hey srv its me')
+})
 
 client.connect('localhost', 9999)
+server.listen(9999)
 
-client.send('Hey srv its me')
 
-client.disconnect()
 
-server.stop()
+
